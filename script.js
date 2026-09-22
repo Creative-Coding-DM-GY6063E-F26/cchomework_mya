90e2af2c65e05b17117a4645a2bf78fba6ff1ced
mocha404 <mmc10196@nyu.edu>
2026-09-22 01:18:55 -0400

    git commit

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ const quotes = [
   { text: "iterative and experimental", source: "Scooby" },
   { text: "a community of practice", source: "Seth" },
   { text: "an opportunity to stick it to the man", source: "Shifty-eyed Susan" }
+  { text: "computation as an expressive, analytical, and visualizing medium", source: "NYU Steinhardt" },
+  { text: "code and programming to create more expressive, designed web work", source: "Wix Studio Blog" },
+  { text: "where self-expression and logic meet to form a beautiful kind of modern art", source: "Egyptian Streets" },
+  { text: "using code to express creativity, much like drawing but with programming", source: "GitHub (avishka964)" },
+  { text: "discovery, variation, and unexpected exploration", source: "Tim Rodenbröcker" },
+  { text: "curiosity fueling experimentation and discovery", source: "Patrik Hübner" },
+  { text: "aesthetics and emotional impact, not just efficiency", source: "SMU Meadows School of the Arts" },
+  { text: "blending creative technique with software engineering", source: "UCLA CCI Diploma" },
+  { text: "an ongoing dialogue between coder and computer", source: "Site of Sites" },
+  { text: "expressivity beyond pure engineering", source: "Aesthetic Programming" }
 ];
 // no need to edit anything below this line! 
 // if you have made an error, you can check your history to see what might have gone wrong
@@ -24,7 +34,7 @@ function setup() {
 
 function pickQuote() {
   // take a random number and use that to identify what quote to use
-  current = quotes[floor(quotes.length)]; 
+  current = quotes[floor(random(quotes.length))]; 
 }
 
 function draw() {
@@ -54,5 +64,5 @@ function newQuote() {
 }
 
 function mousePressed() {
-  newQuote(); /
+  newQuote();
 }
