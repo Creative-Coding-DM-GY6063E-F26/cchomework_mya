93f2e055ea19c9a80a55279a3cfb88fd39e9d3ad
mocha404 <mmc10196@nyu.edu>
2026-09-24 20:04:40 -0400

    Update week 4 files from class repo

--- week4_due_oct1/sketch.js
+++ week4_due_oct1/sketch.js
@@ -1,24 +1,32 @@
-function setup() {
-  createCanvas(400, 400);
+// This is the sketch.js file.
+// Press 's' to export the SVG.
+// Note that p5.js is used in 'global mode'. 
 
+p5.disableFriendlyErrors = true; // keep warnings quiet
+let bDoExportSvg = false; 
 
-noLoop();
-randomSeed(100)
-
+function setup(){
+  // These canvas dimensions are 8.5"x11" at 96 dpi
+  createCanvas(816, 1056); 
 }
 
-function draw() {
-  background(220);
-  circle(random(width), random(height), random(50));
-
+function keyPressed(){
+  if (key == 's'){ 
+    bDoExportSvg = true; 
+  }
 }
 
+function draw(){
+  background(255); 
+  if (bDoExportSvg){
+    beginRecordSvg("myOutput.svg");
+  }
 
-function keyPressed() {
+  // Draw stuff here, such as:
+  line(0,0, mouseX, mouseY); 
 
-  if(key=== 'r'){
-    redraw()
-  } 
-
-
+  if (bDoExportSvg){
+    endRecordSvg();
+    bDoExportSvg = false;
+  }
 }
--- week4_due_oct1/week4_readme.md
+++ week4_due_oct1/week4_readme.md
@@ -10,3 +10,6 @@ loops, arrays, noise
 
 ## Assignment
 Assignment 4: Iteration Generation (Due Oct 1)
+Using the Pen Plotter and p5.plotSVG library, “print at least 3 variations of your Moire effect. More information about pen plotters can be found here: https://github.com/Creative-Coding-DM-GY6063E-F26/CC-F26-ClassResources/blob/main/resources.md#Pen-Plotters
+
+Use the template sketch in this directory as your starting point. 
